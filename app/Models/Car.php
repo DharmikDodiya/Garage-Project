29e5509de95f0593d306bb8567e246a06d0053ff
dharmik <dharmikd@zignuts.com>
2023-04-07 18:52:08 +0530

    create model Car and add relationship

---
+++ app/Models/Car.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+    /**
+     * Fillable Property
+     */
+    protected $fillable =[
+        'owner_id',
+        'company_name',
+        'model_name',
+        'manufacturing_year'
+    ];  
+
+    public function carServicings(){
+        return $this->belongsToMany(ServiceType::class,'car_servicings','car_id','service_type_id')->withPivot('garage_id');
+    }
+}
