574d1119d5915743321c604d87065f84b5ebf5b0
dharmik <dharmikd@zignuts.com>
2023-04-07 18:53:24 +0530

    create pivot model CarServing

---
+++ app/Models/CarServicing.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarServicing extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'garage_id',
+        'car_id',
+        'service_type_id'
+    ];
+}
