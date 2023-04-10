0afb04235b56850b1f2982de8afd53b92fa8c896
dharmik <dharmikd@zignuts.com>
2023-04-10 18:07:25 +0530

    create new model CarServicingJob and add relationship

---
+++ app/Models/CarServicingJob.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarServicingJob extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'car_servicing_id',
+        'mechanic_id',
+        'status',
+        'service_type_id'
+    ];
+}
